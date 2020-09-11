5471ef0d1e107f6d8894ea416c6e4573b2315318
tatsuya.takimoto <seahorse1419.2@gmail.com>
2020-09-12 02:40:47 +0900

    Initial Commit

---
+++ SlideshowApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp2
+//
+//  Created by 瀧本達也 on 2020/09/12.
+//  Copyright © 2020 瀧本達也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
